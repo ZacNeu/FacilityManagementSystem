bff8dfd71a9c89462da9986f3d61eba526127008
V <2000willito@gmail.com>
2022-02-28 00:46:43 -0600

    Updated some methods

--- Facility.java
+++ Facility.java
@@ -2,29 +2,48 @@ import java.io.*;
 import java.util.*;
 
 /* Was thinking if we wanna keep things super simple,
-we could use a bunch of arrays for different information and
+we could use a bunch of array for different information and
  just have to keep track of indexes so we don't need to
  implement more complicated data structures*/
 
 public class Facility {
 
-    // List of facilities
-    public object listFacilities(){
+    String[] facilityNames = {"Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"};
+    String[] facilityStatus = {"Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty", "Empty"};
 
+    // Request list of facilities
+    public object listFacilities(){
+       for(int i = 0; i < facilityNames.length; i++){
+           System.out.println(facilityNames[i]) // Prints all entries in the names list
+       }
     }
 
     // Request existing facility information
-    public object getFacilityInformation(){
-
+    public object getFacilityInformation(String facilityName){
+        for(int i = 0; i < facilityNames.length; i++){
+            if(facilityNames[i].equals(facilityName)){
+                System.out.println(facilityStatus[i]) // Print out information of the given facility
+                /* Add more information to print, not sure
+                what other information to list for the facilities */
+            }
+        }
     }
 
-    // Request the available capacity of facilities to be added
+    // Request the available capacity of facilities
     public object requestAvailableCapacity(){
+        int capacity = 0;
 
+        for(int i = 0; i < facilityNames.length; i++){
+            if(facilityNames[i].equals("Empty")){
+                capacity++;
+            }
+        }
+
+        System.out.print("Facility capacity: " + capacity);
     }
 
     // Add a new facility to the list
-    public object addNewFacility(){
+    public object addNewFacility(String newFacility){
 
     }
 
